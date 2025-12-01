fc24629cd1ae9ce25ffd1e9a154c2b57f776d03e
rivermoooon <speedle71@naver.com>
2025-12-01 17:47:17 +0900

    feat: Network DI 모듈 추가

---
+++ core/network/src/main/java/com/moonsu/assignment/core/network/di/DataSourceModule.kt
@@ -0,0 +1,20 @@
+package com.moonsu.assignment.core.network.di
+
+import com.moonsu.assignment.core.network.impl.RemoteCharacterDataSourceImpl
+import com.moonsu.assignment.data.datasource.RemoteCharacterDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteCharacterDataSource(
+        impl: RemoteCharacterDataSourceImpl,
+    ): RemoteCharacterDataSource
+}
