1cfcd80fbce79fed2c9ffb26e1cef3d665923b9b
rivermoooon <speedle71@naver.com>
2025-12-04 18:43:13 +0900

    refactor: data - CharacterRepositoryImpl PagingConfig 상수화 및 개선

--- data/src/main/java/com/moonsu/assignment/data/impl/CharacterRepositoryImpl.kt
+++ data/src/main/java/com/moonsu/assignment/data/impl/CharacterRepositoryImpl.kt
@@ -18,23 +18,21 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class CharacterRepositoryImpl @Inject constructor(
     private val remote: RemoteCharacterDataSource,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
     @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher,
 ) : CharacterRepository {
-    override fun getCharacters(page: Int): Flow<DataResource<List<Character>>> =
-        flowDataResource {
-            remote.getCharacters(page).toDomainList()
-        }.flowOn(ioDispatcher)
 
     override fun getPagedCharacters(): Flow<PagingData<Character>> {
         return Pager(
             config = PagingConfig(
-                pageSize = 20,
-                initialLoadSize = 40,
-                prefetchDistance = 2,
+                pageSize = PAGE_SIZE,
+                initialLoadSize = INITIAL_LOAD_SIZE,
+                prefetchDistance = PREFETCH_DISTANCE,
                 enablePlaceholders = false,
             ),
             pagingSourceFactory = {
@@ -44,7 +42,9 @@ class CharacterRepositoryImpl @Inject constructor(
                 )
             },
         ).flow
-            .map { pagingData -> pagingData.map { it.toDomain() } }
+            .map { pagingData ->
+                pagingData.map { it.toDomain() }
+            }
             .flowOn(defaultDispatcher)
     }
 
@@ -55,6 +55,12 @@ class CharacterRepositoryImpl @Inject constructor(
 
     override fun searchCharacters(name: String): Flow<DataResource<List<Character>>> =
         flowDataResource {
-            remote.searchCharacters(name).toDomainList()
+            remote.searchCharacters(name).results.toDomainList()
         }.flowOn(ioDispatcher)
+
+    companion object {
+        private const val PAGE_SIZE = 20
+        private const val INITIAL_LOAD_SIZE = 40
+        private const val PREFETCH_DISTANCE = 5
+    }
 }
