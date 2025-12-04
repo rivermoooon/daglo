ab40a21d6d90f7e8ab5c0de668af6c3b8cf28753
rivermoooon <speedle71@naver.com>
2025-12-04 17:23:13 +0900

    feat: data - RemoteCharacterDataSource에 searchCharacters 메서드 추가

--- data/src/main/java/com/moonsu/assignment/data/remote/RemoteCharacterDataSource.kt
+++ data/src/main/java/com/moonsu/assignment/data/remote/RemoteCharacterDataSource.kt
@@ -5,4 +5,5 @@ import com.moonsu.assignment.data.model.CharacterEntity
 interface RemoteCharacterDataSource {
     suspend fun getCharacters(page: Int): List<CharacterEntity>
     suspend fun getCharacter(id: Int): CharacterEntity
+    suspend fun searchCharacters(name: String): List<CharacterEntity>
 }
