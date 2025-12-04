a222271d5f91feb63f4dca5e9fb69064e73393ea
rivermoooon <speedle71@naver.com>
2025-12-04 18:43:16 +0900

    refactor: domain - CharacterRepository getCharacters 메서드 제거

--- domain/src/main/java/com/moonsu/assignment/domain/repository/CharacterRepository.kt
+++ domain/src/main/java/com/moonsu/assignment/domain/repository/CharacterRepository.kt
@@ -6,7 +6,6 @@ import com.moonsu.assignment.domain.model.Character
 import kotlinx.coroutines.flow.Flow
 
 interface CharacterRepository {
-    fun getCharacters(page: Int): Flow<DataResource<List<Character>>>
     fun getPagedCharacters(): Flow<PagingData<Character>>
     fun getCharacter(id: Int): Flow<DataResource<Character>>
     fun searchCharacters(name: String): Flow<DataResource<List<Character>>>
